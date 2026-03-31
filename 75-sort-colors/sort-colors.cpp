3fbbfd2a742bb682ca92521833d6d02158d4c631
Solver-rajat <kumarajat.910@gmail.com>
2026-03-31 19:52:09 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (77.92%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
-    void sortColors(vector<int>& a) {
+    void sortColors(vector<int>& a) 
+    {
         int n  = a.size();
 
-        int low = 0, mid = 0, high = n;
+        int low = 0, mid = 0, high = n-1;
 
-       int count0 = 0, count1 = 0, count2 = 0; // Initialize counters for 0s, 1s, and 2s
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i] == 0)
+        while (mid <= high)
         {
-            count0++; // Increment count of 0s
+            if (a[mid] == 0)
+            {
+                swap(a[low], a[mid]);
+                low++;
+                mid++;
+            }
+            else if (a[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(a[mid], a[high]);
+                high--;
+            }
         }
-        else if (a[i] == 1)
-        {
-            count1++; // Increment count of 1s
-        }
-        else
-        {
-            count2++; // Increment count of 2s
-        }
     }
-    int index = 0; // Initialize index to keep track of the position in the array
-    for (int i = 0; i < count0; i++)
-    {
-        a[i] = 0;
-    }
-
-    for (int i = count0; i < count0+count1; i++)
-    {
-        a[i] = 1;
-
-    }
-    
-    for (int i = count0+count1; i < n; i++)
-    {
-        a[i] = 2;
-    }
-
-   
-}
-    };
+};
