4477f8a8339a421fbb6982f82ecfc3c4d99af2e8
Solver-rajat <kumarajat.910@gmail.com>
2026-07-10 23:13:49 +0530

    Time: 86 ms (24.23%) | Memory: 53.8 MB (5.53%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) 
     {
-        int count = 0;
         int n = nums.size();
-        for (int i = 0; i < n; i++)
+        map<int, int> mpp;
+        mpp[0] = 1;
+        int presum = 0;
+        int count = 0;     
+
+        for(int i = 0; i < n; i++)
         {
-            int sum = 0;
-            for (int j = i; j < n; j++)
-            {
-                sum += nums[j];
-                if (sum == k)
-                {
-                    count++;
-                }
-            }
-        }
+            presum += nums[i];
+            int rem = presum - k;
+
+            count += mpp[rem];
+            mpp[presum]++;
+        }   
         return count;
     }
 };
