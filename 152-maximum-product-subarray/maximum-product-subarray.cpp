4e7e5c077d1c8aabe3b9e064a4245060fb67c696
Solver-rajat <kumarajat.910@gmail.com>
2026-07-29 20:28:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (60.89%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -3,16 +3,20 @@ public:
     int maxProduct(vector<int>& v) 
     {
         int n = v.size();
-        int maxpro = v[0];
+        int maxpro = INT_MIN;
+        int pre = 1, suff = 1;
         for(int i = 0; i < n; i++)
         {
-            int pro = 1;
-            for(int j = i; j < n; j++)
-            {
-                pro *= v[j];
-                maxpro = max(maxpro, pro);
-            }
+            if(pre == 0)
+                pre = 1;
+            
+            if(suff == 0)
+                suff = 1;
 
+            pre = pre * v[i];
+            suff = suff * v[n-i-1];
+
+            maxpro = max(maxpro, max(pre, suff));
         }
         return maxpro;
     }
