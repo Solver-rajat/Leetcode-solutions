2e436a895984fe51417cb441e336a8033ed796a7
Solver-rajat <kumarajat.910@gmail.com>
2026-08-11 22:43:08 +0530

    Time: 86 ms (15.52%) | Memory: 113.4 MB (14.67%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& a) 
+    {
+        int n = a.size();
+
+        unordered_map<int, int> mpp;
+
+        for(int i = 0; i < n; i++)
+        {
+            mpp[a[i]]++;
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            if(mpp[a[i]] > 1)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
