2277ddde31d44722e09df8dda564a6ca42f00f82
Solver-rajat <kumarajat.910@gmail.com>
2026-08-19 22:59:24 +0530

    Time: 12 ms (49.89%) | Memory: 35 MB (54.51%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,52 +1,33 @@
 class Solution {
 public:
-    int daysReq(vector<int>& weights, int cap)
-    {
-        int n = weights.size();
-        int days = 1; // Start with one day
-        int currentload = 0; // Current load of the ship
+    int shipWithinDays(vector<int>& weights, int days) {
+        int left = *max_element(weights.begin(), weights.end());
+        int right = accumulate(weights.begin(), weights.end(), 0);
 
-        for (int i = 0; i < n; i++)
-        {
-            if (weights[i] > cap) // If a single package exceeds capacity, it's impossible
-                return INT_MAX;
-
-            if (currentload + weights[i] > cap) // If adding this package exceeds capacity, increment days
-            {
-                days++;
-                currentload = weights[i]; // Start new load with current package
-            }
-
-            else
-            {
-                currentload += weights[i]; // Add package to current load
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canShip(weights, days, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
 
-        return days;
+        return left;
     }
-    int shipWithinDays(vector<int>& weights, int D)
-    {
-        int n = weights.size();
-        int low = *max_element(weights.begin(), weights.end());
-        int high = accumulate(weights.begin(), weights.end(), 0);
-        int ans = high;
 
-        while(low <= high)
-        {
-            int mid = low + (high - low)/2;
-            int days = daysReq(weights, mid);
+private:
+    bool canShip(vector<int>& weights, int days, int capacity) {
+        int daysNeeded = 1, currentLoad = 0;
 
-            if (days <= D)
-            {
-                ans = mid; // Update ans to the current mid
-                high = mid - 1; // Try to find a smaller capacity
+        for (int w : weights) {
+            if (currentLoad + w > capacity) {
+                daysNeeded++;
+                currentLoad = 0;
             }
-            else
-            {
-                low = mid + 1; // Increase the capacity
-            }
+            currentLoad += w;
         }
-        return ans;
+
+        return daysNeeded <= days;
     }
 };
