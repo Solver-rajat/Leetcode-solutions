9232cc8758fe92c42314211b7f4506b1f49bbb73
Solver-rajat <kumarajat.910@gmail.com>
2026-09-17 21:22:05 +0530

    Time: 11 ms (15.55%) | Memory: 8.5 MB (51.02%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) 
+    {
+        int ans = 0;
+        for(int i = 0; i <= x; i++)    
+        {
+            if((long long) i*i <= x)
+                ans = i;
+            
+            else
+                break;
+        }
+
+        return ans;
+    }
+};
